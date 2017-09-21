e4b38af0c74a3f1a4880731834c9077ad05f706c
kboruff <kboruff@gmail.com>
2017-09-21 03:00:20 -0700

    fixed out of order variable entry
    Need to find a way to truncate options over the default number rather than allow them to overwrite entered variables

--- sample.py
+++ sample.py
@@ -7,32 +7,53 @@ import string
 #cd dockerdata/wordbot_testing
 
 #Tuples containing setting names, note that additional variables can be added into the training arguments and sample options list and it will update the rest automatically
-training_arguments = {'prime_text' : "" , 'model': 0, 'seed' : 0, 'sample': 0, 'length': 0, 'temperature': 0, 'gpu': 0, 'opencl': 0, 'verbose': 0, 'skip_unk': 0, 'input_loop' : 0, 'word_level' : 0}
-sample_options = (' -primetext', ' -model', ' -seed', ' -sample', ' -length', ' -temperature', ' -gpuid', ' -opencl', ' -verbose', ' -skip_unk', ' -input_loop', ' -word_level')
+training_arguments = {'primetext' : "" , 'model': 0, 'seed' : 0, 'sample': 0, 'length': 0, 'temperature': 0, 'gpu': 0, 'opencl': 0, 'verbose': 0, 'skip_unk': 0, 'input_loop' : 0, 'wordlevel' : 0}
+sample_options = (' -primetext', ' -model', ' -seed', ' -sample', ' -length', ' -temperature', ' -gpuid', ' -opencl', ' -verbose', ' -skip_unk', ' -input_loop', ' -wordlevel')
 max_args = len(sample_options)
 args = len(sys.argv)
 arguments = []
 sample_commandline = []
 
-
 def parseArguments (args, arguments):
-	if args >= 1:
-		print('%s sys args detected'%args)
-	if args > max_args:
-		'''Truncates args to the maximum arguments possible in sampling'''
-		print('Actually, too many arguments detected. Truncating to %s.'%max_args)
-		args = max_args + 1
-	if args == 0:
+
+	'''add a step to match -names in the commandline for explicitly given variables here'''
+	if args == 1:
 		print('No arguments detected. Defaults chosen')
+	if args >= 2:
+		print('1 or more sys args detected')
 	''' Enters commandline arguments into a list'''
 	for i in range(args):
 		arguments.append(sys.argv[i])
-	arguments.remove('sample.py')
+	arguments.remove('sample.py')				
+	print(arguments)
 	args = len(arguments)
-	if args > 0:
-		for i in range(args):
-			currentOpt = sample_options[i].replace(' -', "")
-			training_arguments[str(currentOpt) = arguments[i]]
+	
+	''' loop through every argument, see if it is in the default argument list, then enter it into the correct option in the dictionary, so people have individual non-sequence locked variable flexibility'''
+	
+	for i in range(len(arguments)): #for every argument entered
+		for z in range(max_args):
+			for y in range(max_args): # for every element in the sample options list
+				if len(arguments) > 0:
+					print(str(len(arguments)) + ' remaining.')
+					currentOpt = sample_options[y].replace(' -', "")
+					if str(arguments[i].replace('-', "")) == currentOpt:
+						training_arguments[currentOpt] = arguments[i+1]
+						del arguments[i]
+						del arguments[i]
+					else:
+						training_arguments[currentOpt] = arguments[i]
+						del arguments[i]
+
+	
+	''' correct problem where entering variables like wordlevel first means a bad index range is returned. '''
+	
+	args = len(arguments)
+	
+	if args > max_args:
+		'''Truncates args to the maximum arguments possible in sampling'''
+		print('Actually, too many arguments detected. Truncating to %s.'%max_args)
+		args = max_args + 1			
+	
 	return args, arguments, training_arguments
 
 def commandLine(args, sample_commandline):
@@ -40,11 +61,9 @@ def commandLine(args, sample_commandline):
 	sample_commandline= 'th sample.lua '
 	for i in range(args):
 		sample_commandline= sample_commandline+ " " + sample_options[i] + " " + arguments[i]
-	print(sample_commandline)
 	return sample_commandline
 
 print(training_arguments.items)
-
 
 args, arguments, training_arguments = (parseArguments(args, arguments))
 print('Current length is %s'%len(arguments))
@@ -57,8 +76,8 @@ print(training_arguments.items())
 
 
 '''
-def sample(primetext, length, model = 'word', seed = '123', sample = 1, temperature = 1, gpu = -1, opencl = 0, verbose = 1, skip_unk = 0, input_loop = 0, word_level = 1):
-	print(primetext, length, model, seed, sample, temperature, gpu, opencl, verbose, skip_unk, input_loop, word_level)
+def sample(primetext, length, model = 'word', seed = '123', sample = 1, temperature = 1, gpu = -1, opencl = 0, verbose = 1, skip_unk = 0, input_loop = 0, wordlevel = 1):
+	print(primetext, length, model, seed, sample, temperature, gpu, opencl, verbose, skip_unk, input_loop, wordlevel)
 	
 	if primetext == "":
 		primetext = random.choice(string.ascii_uppercase)
